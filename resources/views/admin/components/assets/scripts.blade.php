b56858d2e8bb53fd44ff13055b7d0e19b0c0d1aa
Stefan Oljacic <djedpool@gmail.com>
2020-02-27 18:22:25 +0100

    Adding larrycms-template to laravel.

---
+++ resources/views/admin/components/assets/scripts.blade.php
@@ -0,0 +1,5 @@
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('admin/js/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('admin/js/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('admin/js/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('admin/js/app.js') }}"></script>
